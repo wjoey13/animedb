2d6ecd15c5b9834ac9db881eb68f80f82996c837
joseph walter <joeywalter@josephs-MacBook-Pro.local>
2019-02-18 19:10:53 -0700

    added js file, cors and body-parser :bowtie:

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 const express = require('express');
 const app = express();
+
+const port = process.env.PORT || 1389
+
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.listen(port, () => {
+    console.log(`want to know what port is active? It's port ${port}`);
+});
